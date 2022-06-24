1e521f28c098b08c5957136bacda8f30d8c28a19
Jackie Chung <jackiechung1214@gmail.com>
2022-06-24 16:55:15 +0800

    add content

--- 取得子網頁內容.py
+++ 取得子網頁內容.py
@@ -2,23 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
+for page in range(1,4):
+    response=requests.get(f"https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt?page={page}")
 
-response=requests.get("https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt")
+    soup=BeautifulSoup(response.text,'lxml')
+    titles=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
 
-soup=BeautifulSoup(response.text,'lxml')
-titles=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
+    title_list=[]
+    for title in titles:
+        title_list.append(title.getText())
 
-title_list=[]
-for title1 in titles:
-    title_list.append(title1.getText())
+    urls=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
+    tag_list=[]
+    for url in urls:
+        sub_response=requests.get(url.get('href'))
+        sub_soup=BeautifulSoup(sub_response.text,'lxml')
+        tags=sub_soup.find_all('li',{'class':'bbc-1msyfg1 eqijop70'})
+        for tag in tags:
+            tag_list.append(tag.find('a').getText())
 
-urls=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
-tag_list=[]
-for url in urls:
-    sub_response=requests.get(url.get('href'))
-    sub_soup=BeautifulSoup(sub_response.text,'lxml')
-    tags=sub_soup.find_all('li',{'class':'bbc-1msyfg1 eqijop70'})
-    for tag in tags:
-        tag_list.append(tag.find('a').getText())
-
-print(tag_list)
+    print(f"第{page}頁")
+    print(title_list)
+    print(tag_list)
