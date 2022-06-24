10b6099752428e0c06e71105d800489b2070376d
Jackie Chung <jackiechung1214@gmail.com>
2022-06-24 20:15:06 +0800

    add content

---
+++ 取得多頁網頁資料.py
@@ -0,0 +1,29 @@
+import grequests
+from bs4 import BeautifulSoup
+import time
+
+start_time=time.time()
+for page in range(1,4):
+    response=requests.get(f"https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt?page={page}")
+
+    soup=BeautifulSoup(response.text,'lxml')
+    titles=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
+
+    title_list=[]
+    for title in titles:
+        title_list.append(title.getText())
+
+    urls=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
+    tag_list=[]
+    for url in urls:
+        sub_response=requests.get(url.get('href'))
+        sub_soup=BeautifulSoup(sub_response.text,'lxml')
+        tags=sub_soup.find_all('li',{'class':'bbc-1msyfg1 eqijop70'})
+        for tag in tags:
+            tag_list.append(tag.find('a').getText())
+
+    print(f"第{page}頁")
+    print(title_list)
+    print(tag_list)
+end_time=time.time()
+print(f'花費{end_time-start_time}秒')
