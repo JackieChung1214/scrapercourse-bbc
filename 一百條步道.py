7f4ef9923e21fee025ed5f3ca2b7ad8dc6552034
Jackie Chung <jackiechung1214@gmail.com>
2022-06-24 20:17:18 +0800

    add content

--- 一百條步道.py
+++ 一百條步道.py
@@ -4,7 +4,15 @@ from bs4 import BeautifulSoup
 response=requests.get('https://hiking.biji.co/index.php?q=trail&type=%E7%99%BE%E5%A4%A7%E5%BF%85%E8%A8%AA%E6%AD%A5%E9%81%93&page=1')
 soup=BeautifulSoup(response.text,'lxml')
 titles=soup.find_all('a',{'class':'text-current'})
+webs=soup.find_all('a',{'class':'w-56'})
+
 title_list=[]
 for title in titles:
     title_list.append(title.getText())
 print(title_list)
+
+web_list=[]
+for web in webs:
+    web_list.append('https://hiking.biji.co/'+web.get('href'))
+print(web_list)
+
