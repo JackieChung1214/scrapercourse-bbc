9d0a49778540a79281822f83d5aa3b2bc4ade47c
Jackie Chung <jackiechung1214@gmail.com>
2022-06-24 16:25:53 +0800

    3.4批次爬取子網頁的資料

--- 取得子網頁內容.py
+++ 取得子網頁內容.py
@@ -13,6 +13,12 @@ for title1 in titles:
     title_list.append(title1.getText())
 
 urls=soup.find_all('a',{'class':'bbc-uk8dsi emimjbx0'})
-print(urls)
+tag_list=[]
 for url in urls:
-    print(url.get('href'))
+    sub_response=requests.get(url.get('href'))
+    sub_soup=BeautifulSoup(sub_response.text,'lxml')
+    tags=sub_soup.find_all('li',{'class':'bbc-1msyfg1 eqijop70'})
+    for tag in tags:
+        tag_list.append(tag.find('a').getText())
+
+print(tag_list)
